9598aee1f08364a2669404b134533ed410e7eebf
mopeters8 <mopeters8@gmail.com>
2023-09-12 14:50:04 -0700

    moved index to api for vercel.

--- api/index.js
+++ api/index.js
@@ -2,16 +2,16 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const authRoute = require("./routes/auth");
-const userRoute = require("./routes/users");
-const postRoute = require("./routes/posts");
-const catRoute = require("./routes/categories");
+const authRoute = require("../routes/auth");
+const userRoute = require("../routes/users");
+const postRoute = require("../routes/posts");
+const catRoute = require("../routes/categories");
 const multer = require("multer");
 const path = require("path");
 
 dotenv.config();
 app.use(express.json());
-app.use("/images", express.static(path.join(__dirname, "/images")));
+app.use("/images", express.static(path.join(__dirname, "../images")));
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -23,7 +23,7 @@ mongoose
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "images");
+    cb(null, "../images");
   },
   filename: (req, file, cb) => {
     cb(null, req.body.name);
